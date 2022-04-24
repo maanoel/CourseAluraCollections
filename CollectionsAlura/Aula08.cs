d1b697825340abdcb5e62825752d58f6c49eb4da
maanoel <manoelvitorbrito@gmail.com>
2022-04-24 20:47:37 -0300

    revisando mais sobre filas

--- CollectionsAlura/Aula08.cs
+++ CollectionsAlura/Aula08.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CollectionsAlura
 {
@@ -8,7 +9,31 @@ namespace CollectionsAlura
 
     public void Executando()
     {
+      //Primeiro a entrar, primeiro a sair
+      //FIRST IN FIRST OUT
       Enfileirar("Van");
+      Enfileirar("Kombi");
+      Enfileirar("Guincho");
+      Enfileirar("PickUp");
+      Desenfileirar();
+      Desenfileirar();
+      Desenfileirar();
+
+    }
+
+    private void Desenfileirar()
+    {
+      //OPTAR POR USAR O ANY SEMPRE NO LUGAR DO .COUNT
+      if (!pedagio.Any()) return;
+
+      if (pedagio.Peek() == "Guincho") {
+        System.Console.WriteLine("Guincho fazendo o pagamento");
+      }
+
+      string veiculo = pedagio.Dequeue();
+      System.Console.WriteLine($"Saiu da fila  {veiculo}");
+
+      FilaAtual();
     }
 
     private void Enfileirar(string nomeVeiculo)
